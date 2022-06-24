9ae380d836abb4820bceee37cb7f781f224be1a8
David Kim <davidykim@Davids-MacBook-Pro-2.local>
2022-06-23 23:59:21 -0400

    asda'

--- application.py
+++ application.py
@@ -6,6 +6,14 @@ application = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@application.route('/davidscock')
+def david():
+    return "Is fat"
+
+@application.route('/jah')
+def davidd():
+    return "Get jahb8ed khed"
+
 
 if __name__ == "__main__":
-    application.run(debug=True, port=5000) #turn debug off for prodcution deployment
+    application.run(debug=True, port=5001) #turn debug off for prodcution deployment
